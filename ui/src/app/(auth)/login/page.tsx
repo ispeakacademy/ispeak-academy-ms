27f3ae20b169c2352e19f2967110c63fdb56d51e
sammykirigha <dkirigha18@gmail.com>
2026-03-11 14:01:33 +0300

    removed register link

--- ui/src/app/(auth)/login/page.tsx
+++ ui/src/app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
 import LoginForm from '@/components/forms/login-form';
 import type { Metadata } from 'next';
-import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Sign In | MasomoDash',
@@ -21,15 +20,6 @@ export default function LoginPage() {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Or{' '}
-            <Link
-              href="/register"
-              className="font-medium text-blue-600 hover:text-blue-500"
-            >
-              Create a new account
-            </Link>
-          </p>
         </div>
         <LoginForm />
       </div>
